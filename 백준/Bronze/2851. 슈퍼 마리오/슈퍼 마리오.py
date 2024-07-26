7ae6a79eb341e1f7a54783e1fd7f1bcfc180d00c
yunjii <162238479+itsmeyji@users.noreply.github.com>
2024-07-26 13:43:29 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
@@ -0,0 +1,38 @@
+hap = 0
+i = 0
+num = []
+cndi = []
+
+for _ in range(10):
+    num.append(int(input()))
+
+while True :
+    if i>=len(num):
+        break
+    
+
+    hap += num[i]
+
+    if hap <= 100 :
+        i+=1
+        continue
+    else : 
+        cndi.append(hap)
+        cndi.append(hap-num[i])
+        break
+if len(cndi)==0:
+    print(hap)
+else :
+    if (cndi[0]-100 < 100-cndi[1]):
+        print(cndi[0])
+    elif (cndi[0]-100 == 100-cndi[1]):
+        print(cndi[0])
+    else :
+        print(cndi[1])
+
+
+
+#처음에 10~11행을 잘못 처리했어서 오류가 발생했음
+#i가 리스트의 길이를 초과하지 않도록 체크해야하는데 이 부분을 놓침
+#근데 사실 없어도 되는거 아닌가 싶긴한데
+#일단 저 부분은 숫자를 다 더해도 100이 되지 않는 경우를 고려한 것.
