3f10a5dce898bfb2ede344662dae072a59e0b239
yunjii <162238479+itsmeyji@users.noreply.github.com>
2024-07-31 12:44:21 +0900

    [Silver V] Title: 돌 게임, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -0,0 +1,14 @@
+stn = int(input())
+cnt = 0
+
+while stn :
+    cnt += 1
+    if stn < 3 : 
+        stn -= 1
+    else :
+        stn -= 3
+
+if cnt % 2 ==1 :
+    print("SK")
+elif cnt %2 ==0 :
+    print("CY")
